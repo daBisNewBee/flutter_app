1f243a0470bb935774a9ed0cdbd31d36924be045
liuwenbin <liuwb@koal.com>
2020-01-25 00:54:56 +0800

    编写您的第一个 Flutter App

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,134 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(HelloApp());
+//void main() => runApp(MyApp());
+
+class HelloApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'this is title.....',
+      theme: new ThemeData(
+        primaryColor: Colors.pink
+      ),
+      home: new RandomWords(),
+    );
+  }
+
+/*
+  @override
+  Widget build(BuildContext context) {
+//    final wordPair = new WordPair.random();
+    return new MaterialApp(
+      title: '这里是标题',
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text('这里是AppBar的Title'),
+        ),
+          body: new Center(
+            child: new RandomWords(),
+//            child: new Text(wordPair.asPascalCase),
+//            child: new Text('Hello World'),
+          )
+      ),
+    );
+  }
+   */
+
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return new RandomWordsState();
+  }
+}
+
+class RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final TextStyle _biggerFont = new TextStyle(fontSize: 18.0);
+  final _saved = new Set<WordPair>();
+
+  @override
+  Widget build(BuildContext context) {
+//    final wordPair = new WordPair.random();
+//    return new Text(wordPair.asPascalCase);
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('this is title......'),
+        actions: <Widget>[
+          new IconButton(icon: new Icon(Icons.list), onPressed: _pushSaved)
+        ],
+      ),
+      body: _buildSuggestions(),
+    );
+  }
+
+  void _pushSaved() {
+    print('RandomWordsState._pushSaved');
+    Navigator.of(context).push(new MaterialPageRoute(
+      builder: (context) {
+        final tiles = _saved.map((pair) {
+          return new ListTile(
+            title: new Text(
+              pair.asPascalCase,
+              style: _biggerFont,
+            ),
+          );
+        });
+        final divided =
+            ListTile.divideTiles(context: context, tiles: tiles).toList();
+        return new Scaffold(
+          appBar: new AppBar(
+            title: new Text('Saved Suggestions'),
+          ),
+          body: new ListView(children: divided),
+        );
+      },
+    ));
+  }
+
+  Widget _buildSuggestions() {
+    return new ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: (context, i) {
+          if (i.isOdd) return new Divider();
+
+          final index = i ~/ 2;
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10));
+          }
+          return _buildRow(_suggestions[index]);
+        });
+  }
+
+  Widget _buildRow(WordPair pair) {
+    final alreadySaved = _saved.contains(pair);
+
+    return new ListTile(
+      title: new Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+      trailing: new Icon(
+//        alreadySaved ? Icons.star:Icons.star_border,
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ),
+      onTap: () {
+        setState(() {
+          print("setState:" + alreadySaved.toString());
+          if (alreadySaved) {
+            _saved.remove(pair);
+          } else {
+            _saved.add(pair);
+          }
+        });
+      },
+    );
+  }
+}
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
