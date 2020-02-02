d58b36d904f2e8e715d2e23a57217cf78be7bdb8
liuwenbin <liuwb@koal.com>
2020-02-02 21:25:18 +0800

    MethodChannel提供了方法调用的通道

--- android/app/src/main/java/xmly/com/flutter_app/MainActivity.java
+++ android/app/src/main/java/xmly/com/flutter_app/MainActivity.java
@@ -1,14 +1,90 @@
 package xmly.com.flutter_app;
 
+import android.content.ContextWrapper;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+
 import io.flutter.embedding.android.FlutterActivity;
 import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugins.GeneratedPluginRegistrant;
+import io.flutter.view.FlutterView;
 
 public class MainActivity extends FlutterActivity {
+
+    //channel的名称，由于app中可能会有多个channel，这个名称需要在app内是唯一的。
+    private static final String CHANNEL = "samples.flutter.io/battery";
+
+    private FlutterView mFlutterView;
+    private MethodChannel mMethodChannel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 
     @Override
     public void configureFlutterEngine(FlutterEngine flutterEngine) {
         super.configureFlutterEngine(flutterEngine);
         GeneratedPluginRegistrant.registerWith(flutterEngine);
+        mMethodChannel = new MethodChannel(flutterEngine.getDartExecutor(), CHANNEL);
+        mMethodChannel.setMethodCallHandler(new MethodChannel.MethodCallHandler() {
+            @Override
+            public void onMethodCall(MethodCall methodCall, MethodChannel.Result result) {
+                if (methodCall.method.equals("getBatteryLevel")) {
+                    int batterLevel = getBatteryLevel();
+                    if (batterLevel != -1) {
+                        result.success(batterLevel);
+                    } else {
+                        result.error("UNAVAIABLE", "无法获取电池信息", null);
+                    }
+                }
+                else if (methodCall.method.equals("invokeFlutterMethod")) {
+                    invokeFlutterMethod();
+                }
+                else {
+                    result.notImplemented();
+                }
+            }
+        });
+    }
+
+    private void invokeFlutterMethod() {
+        mMethodChannel.invokeMethod("getName", null, new MethodChannel.Result() {
+            @Override
+            public void success(Object o) {
+                Log.d("flutter", "Native. success: " + o);
+            }
+
+            @Override
+            public void error(String s, String s1, Object o) {
+                Log.d("flutter", "Native error: " + s);
+            }
+
+            @Override
+            public void notImplemented() {
+                Log.d("flutter", "notImplemented.");
+            }
+        });
+    }
+
+    private int getBatteryLevel() {
+        int batteryLevel = -1;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            BatteryManager batteryManager = (BatteryManager) getSystemService(BATTERY_SERVICE);
+            batteryLevel = batteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY);
+        } else {
+            Intent intent = new ContextWrapper(getApplicationContext()).
+                    registerReceiver(null, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
+            batteryLevel = (intent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1) * 100) /
+                    intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
+        }
+
+        return batteryLevel;
     }
 }
--- lib/record.dart
+++ lib/record.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_sound/flutter_sound.dart';
 import 'package:flutter_sound/ios_quality.dart';
@@ -10,8 +11,14 @@ class RecordApp extends StatelessWidget {
   StreamSubscription _streamSubscription;
   String _audioPath;
 
+  // channel的名称要和Native端的一致
+  // MethodChannel提供了方法调用的通道
+  static const platform = const MethodChannel("samples.flutter.io/battery");
+
   @override
   Widget build(BuildContext context) {
+    platform.setMethodCallHandler(platformCallHandler);
+
     return MaterialApp(
       title: '录音',
       home: Scaffold(
@@ -33,6 +40,12 @@ class RecordApp extends StatelessWidget {
               new RaisedButton(
                   child: Text('停止播放'),
                   onPressed: _stopPlay),
+              new RaisedButton(
+                child: Text('Flutter调用Native方法'),
+                onPressed: _getMsgFlutterFromNative),
+              new RaisedButton(
+                child: Text('Native调用Flutter方法'),
+                  onPressed: _getMsgNativeFromFlutter),
             ],
           ),
         ),
@@ -79,5 +92,27 @@ class RecordApp extends StatelessWidget {
    _stopPlay() async{
     String result = await flutterSound.stopPlayer();
     print('_stopPlay:' + result);
+  }
+
+  _getMsgFlutterFromNative() async {
+    final int result = await platform.invokeMethod("getBatteryLevel");
+    print('getBatteryLevel: $result');
+  }
+
+  Future<dynamic> platformCallHandler(MethodCall call) async {
+    String retStr;
+    switch(call.method) {
+      case "getName":
+        retStr = "This is String from Flutter World.";
+        break;
+      default:
+        retStr = "This is default String from flutter.";
+        break;
+    }
+    return retStr;
+  }
+
+   _getMsgNativeFromFlutter() async {
+     await platform.invokeMethod('invokeFlutterMethod');
   }
 }
