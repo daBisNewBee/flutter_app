186b5fd31bc4e61a6abba14c8006b33334a03e9e
wenbin.liu <wenbin.liu@ximalaya.com>
2020-09-05 02:20:08 +0800

    flutter的线程管理模型： 4个重要的runner 单线程模型 isolate 的通信方式：普通、单向、双向

--- lib/forAndroid.dart
+++ lib/forAndroid.dart
@@ -7,6 +7,40 @@ import "dart:convert";
 import 'package:sprintf/sprintf.dart';
 import 'package:http/http.dart' as http;
 
+/*
+* flutter的线程模型:
+* 4个重要的runner：
+* 1. Platform Task Runner(与平台通信，不卡顿)
+*    负责 Flutter 中间层和 android 进程之间的通信
+*    他的卡顿不会造成 flutter 卡顿，但是线程的阻塞可能会被系统强杀
+*
+* 2. UI Task Runner(提供绘制数据，卡顿)
+*    会执行 Flutter的 root Isolate代码，负责 Widgets tree 的构建，解析，渲染的发起
+*    这个必然造成卡顿了
+*
+* 3. GPU Task Runner(实际的绘制操作，卡顿)
+*    wieght tree 渲染
+*    也会卡顿
+*
+* 4. IO Task Runner(耗时操作，不卡顿)
+*    从磁盘读取并压缩压缩的图片格式，给GPU喂数据
+*    这个不会卡顿
+*
+* 参考：
+* Flutter 的线程管理
+* https://blog.csdn.net/alitech2017/article/details/81108487
+*
+*
+* 单线程模型：
+* 1. Event Loop 机制
+*
+* 2. 异步任务：
+*   实际有两个队列：微任务队列（Microtask Queue）、事件队列（Event Queue）
+*   前者优先级更高，后者更常用
+*
+* 3. Future: 对Event Queue的一层封装
+*
+* */
 class ForAndroidApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -68,12 +102,14 @@ class _AsyncUIPageState extends State<AsyncUIPage> {
       child:GestureDetector(
         onTap: () {
           print("------------ click: $i");
+          // 最简单的isolate例子
+          loadIsolateHelloWorld();
+          // 单向通信
           loadIsolateSimple();
-//          if(i % 2 == 0) {
-//            loadDataViaPort();
-//          } else{
-//            loadData();
-//          }
+          // 双向通信
+          loadDataViaPort();
+          // 协程方式
+          loadData();
         },
         child:Text(
           sprintf("id:%d title:%s", [widgets[i]["id"], widgets[i]["title"]]),
@@ -99,7 +135,12 @@ class _AsyncUIPageState extends State<AsyncUIPage> {
     });
   }
 
-  // 2. 这个例子的结论：在新建的Isolate中修改i和intObject变量的值后，
+  loadIsolateHelloWorld() async {
+    Isolate.spawn(handle_func, "hello world!!!");
+  }
+
+  // 2. Isolate的单向通信：
+  // 这个例子的结论：在新建的Isolate中修改i和intObject变量的值后，
   // 在main()函数所在Isolate中不能获取到，所以Isolate在内存上是隔离的。
   loadIsolateSimple() async {
     final receive = ReceivePort();
@@ -114,6 +155,26 @@ class _AsyncUIPageState extends State<AsyncUIPage> {
   static int i = 0;
   static IntObject intObject = IntObject();
 
+  /*
+  * "Isolate之间的通信机制"
+  * 1. isolate里是一个event loop（事件循环）
+  * 2. 一旦有事件发生(sendPort.send), 封装消息
+  * 3. 投递消息到另一个isolate
+  * 4. 如何投递？
+  *    有一个map维护 port 与 接受者处理函数(MessageHandler)的对应关系
+  * 5. 既然isolate隔离，为何消息可以投递？
+  *    上述map是 isolate 共享的
+  *
+  * isolate可以隔离的根本原因：
+  * 每个isolate有自己的堆空间，即：堆隔离！
+  * 且内存效率高的原因是，各个堆，有自己的年轻代、老年代，对应不同的gc算法
+  *
+  * 参考：
+  * "Dart中的Isolate"：
+  * https://blog.csdn.net/joye123/article/details/102913497
+  *
+  *
+  * */
   static isolateEntryFunction(SendPort sendPort){
     int counter = 0;
     Timer.periodic(const Duration(seconds: 3), (_){
@@ -128,6 +189,10 @@ class _AsyncUIPageState extends State<AsyncUIPage> {
     });
   }
 
+  static handle_func(msg) {
+    print("打印 msg: $msg");
+  }
+
   /*
   * 由于并没有开启新的线程，只是进行IO中断改变CPU调度，
   * 所以网络请求这样的异步操作可以使用async、await，
@@ -139,7 +204,7 @@ class _AsyncUIPageState extends State<AsyncUIPage> {
   * isolate的出现，线程之间内存"隔离"，可以相互不影响
   *
   * */
-  // 3. isolate 机制 。 TODO： 其实有点懵逼
+  // 3. Isolate的双向通信
   loadDataViaPort() async {
     // 1.
     // 通过spawn新建一个isolate，并绑定静态方法
